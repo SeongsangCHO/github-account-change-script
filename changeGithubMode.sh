369cf35d6ee14e89f93fea48882512554c2de6f2
SeongsangCHO <liasqui31@gmail.com>
2024-02-28 23:00:13 +0900

    잘못된 비교연산자 수정

--- changeGithubMode.sh
+++ changeGithubMode.sh
@@ -1,15 +1,15 @@
-#!bin/bash
+#!/bin/bash
 
 echo  "\033[32m"회사계정을 사용하시려면 work, 개인계정을 사용하시려면 own을 입력해주세요."\033[0m"
 read githubType
 
-if [ "$githubType" == "work" ]
+if [ "$githubType" = "work" ]
 then
   echo "work type"
   git config --global user.email "$COMP_EMAIL"
   git config --global user.name "$COMP_NAME"
   ssh-add ~/.ssh/$COMP_SSH_NAME
-elif [ "$githubType" == "own" ]
+elif [ "$githubType" = "own" ]
 then
   git config --global user.email "$OWN_EMAIL"
   git config --global user.name "$OWN_NAME"
